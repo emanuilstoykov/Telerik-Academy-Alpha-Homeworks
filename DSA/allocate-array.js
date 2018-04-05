f1afdff5be5a3b83a5bfa03fe12d722f9a8ff834
Emanuil Stoykov <emanuilstoykov@yahoo.com>
2018-04-05 17:27:40 +0300

    Create allocate-array.js

---
+++ DSA/allocate-array.js
@@ -0,0 +1,27 @@
+const getGets = (arr) => {
+    let index = 0;
+
+    return () => {
+        const toReturn = arr[index];
+        index += 1;
+        return toReturn;
+    };
+};
+// this is the test
+const test = [
+  '5',
+  '0',
+  '5',
+  '10',
+  '15',
+  '20'
+]
+
+const gets = this.gets || getGets(test);
+const print = this.print || console.log;
+
+const number = +gets();
+
+for(let i = 0,j = 0; j < number ; i +=5, j+=1) {
+  print(i);
+}
